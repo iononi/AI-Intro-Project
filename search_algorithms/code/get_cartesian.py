aecf0c1e2fc5c8cb7967be70066957761b9c2c45
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-12-12 13:30:09 -0600

    modifica valores de tamaño de la imagen y escala

--- search_algorithms/code/get_cartesian.py
+++ search_algorithms/code/get_cartesian.py
@@ -1,7 +1,11 @@
+import image_process as img
+
+original_labyrith = img.load_image("img/borderless.png")
+
 WIDTH = 20.7
 HIGH =20.7
-PIXEL_WIDTH = 375
-PIXEL_HIGH = 375
+PIXEL_WIDTH = original_labyrith.shape[0]
+PIXEL_HIGH = original_labyrith.shape[1]
 
 def pixel_to_cartesian(point:tuple):
     # para este caso la imagen es de 375 x 375 pixeles
@@ -14,6 +18,17 @@ def pixel_to_cartesian(point:tuple):
 file = open('cartesian_path.txt', 'w')
 
 with open('coordenadas.txt', 'r') as f:
+    w = input('Ingresa el ancho: (default es 20.7): ')
+    h = input('Ingresa el alto: (default es 20.7): ')
+
+    try:
+        w = float(w) # si la conversion es posible, usala
+        h = float(h)
+        WIDTH = w
+        HIGH = h
+    except:
+        pass
+
     for line in f:
         x, y = [int(data) for data in line.split(', ')]
         cart_x, cart_y = pixel_to_cartesian((x, y))
